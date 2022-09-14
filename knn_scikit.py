d255cea3589c7491c3b4fbe321937c10119f7d4c
Ethan Sebastian <ethansebastian@Ethans-MacBook-Air.local>
2022-09-14 17:00:44 +0200

    knn compledted more or less

--- knn_scikit.py
+++ knn_scikit.py
@@ -1,12 +1,20 @@
 import pandas as pd
-import sklearn.neigbours as neigh
+import sklearn.neighbors as neigh
+import numpy as np
 
 column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
 iris_data_set = pd.read_csv("iris.csv", names= column_names, header= None)
 
-print(iris_data_set.head())
-print(iris_data_set.tail())
+print(iris_data_set.head())                              # Die ersten fünf Samples
+print(iris_data_set.tail())                              # Die letzten fünf Samples
 print(iris_data_set.describe())
 
-matrix_X = iris_data_set.iloc [0:150 ,  0:4]. values
-vector_y = iris_data_set.iloc [0:150 ,  4]. values
+matrix_X = iris_data_set.iloc [0:149 ,  0:4]. values
+vector_y = iris_data_set.iloc [0:149 ,  4]. values
+
+classifier = neigh.KNeighborsClassifier(3)
+classifier.fit(matrix_X, vector_y)
+
+x = [5.9, 3.0, 5.1, 1.8]
+predicted_class = classifier.predict(x)
+print(predicted_class)
--- matrixberechnungen.py
+++ matrixberechnungen.py
@@ -2,8 +2,8 @@ import numpy as np
 
 x = np.array([[1,2,4],[4,5,6],[7,8,9]])
 print(x)
-y = np.array([[1,1,1],[1,1,1],[1,1,1]])
-print(np.matmul(x,y))
-print(np.transpose(x))
+#y = np.array([[1,1,1],[1,1,1],[1,1,1]])
+#print(np.matmul(x,y))
+#print(np.transpose(x))
 z = np.linalg.inv(x)
 print(np.matmul(x,z))
