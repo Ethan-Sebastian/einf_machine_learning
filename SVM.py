ca8f28fac393d95ba117c1b4926accda5d92abd1
Ethan Sebastian <ethansebastian@Ethans-MacBook-Air.local>
2022-09-14 17:39:17 +0200

    SVM continuation

--- SVM.py
+++ SVM.py
@@ -8,5 +8,10 @@ print(svm_data_set.head())                              # Die ersten fünf Sampl
 print(svm_data_set.tail())                              # Die letzten fünf Samples
 print(svm_data_set.describe())
 
-matrix_X = svm_data_set.iloc [0:12, 0:3]
-vector_y = svm_data_set.iloc [0:12, 3]
+matrix_X = svm_data_set.iloc [0:12, 0:2]. values
+vector_y = svm_data_set.iloc [0:12, 2]. values
+
+svclassifier = svm.SVC(kernel="linear")
+svclassifier.fit(matrix_X, vector_y)
+print("w = ", svclassifier.coef_)
+print("b = ", svclassifier.intercept_)
