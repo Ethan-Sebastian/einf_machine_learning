aec52ed59aae5b3b6eefef8c78fda8d6ba3a4e61
Ethan Sebastian <ethansebastian@Ethans-MacBook-Air.local>
2022-09-16 11:07:37 +0200

    fixed knn_scikit

--- SVM.py
+++ SVM.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sklearn.svm as svm
 
 column_names = ["x-coord.", "y-coord.", "class"]
-svm_data_set = pd.read_csv("SVM.py", names= column_names, header= None)
+svm_data_set = pd.read_csv("abstrakt.csv", names= column_names, header= None)
 
 print(svm_data_set.head())                              # Die ersten fünf Samples
 print(svm_data_set.tail())                              # Die letzten fünf Samples
@@ -15,3 +15,7 @@ svclassifier = svm.SVC(kernel="linear")
 svclassifier.fit(matrix_X, vector_y)
 print("w = ", svclassifier.coef_)
 print("b = ", svclassifier.intercept_)
+
+x = [15, 10]
+predicted_class = svclassifier.predict(x)
+print(predicted_class)
--- knn_scikit.py
+++ knn_scikit.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 import sklearn.neighbors as neigh
+import sklearn.model_selection as modsel
 
 column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
 iris_data_set = pd.read_csv("iris.csv", names= column_names, header= None)
@@ -11,9 +13,17 @@ print(iris_data_set.describe())
 matrix_X = iris_data_set.iloc [0:149 ,  0:4]. values
 vector_y = iris_data_set.iloc [0:149 ,  4]. values
 
-classifier = neigh.KNeighborsClassifier(3)
-classifier.fit(matrix_X, vector_y)
+X_train, X_test, y_train, y_test = modsel.train_test_split(matrix_X, vector_y, test_size=0.2, random_state=0,
+                                                           stratify=vector_y)
 
-x = [5.9, 3.0, 5.1, 1.8]
-predicted_class = classifier.predict(x)
+classifier = neigh.KNeighborsClassifier(n_neighbors=3)
+classifier.fit(X_train, y_train)
+
+accuracy = classifier.score(X_test, y_test)
+print(f"Genauigkeit: {accuracy}")
+
+x = np.array([6.9, 2.9, 5.0, 1.0])
+x_row_vector = x.reshape((1, 4))
+
+predicted_class = classifier.predict(x_row_vector)
 print(predicted_class)
