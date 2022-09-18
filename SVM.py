5e3c762726759f106e71367d11148406ca606d5c
Ethan Sebastian <ethansebastian@Ethans-MacBook-Air.local>
2022-09-18 10:16:51 +0200

    Svm complete

--- SVM.py
+++ SVM.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sklearn.svm as svm
 
+
 column_names = ["x-coord.", "y-coord.", "class"]
 svm_data_set = pd.read_csv("abstrakt.csv", names= column_names, header= None)
 
@@ -15,7 +16,10 @@ svclassifier = svm.SVC(kernel="linear")
 svclassifier.fit(matrix_X, vector_y)
 print("w = ", svclassifier.coef_)
 print("b = ", svclassifier.intercept_)
+print("Number of Support Vectors:", svclassifier.n_support_)
+print("Support Vectors:", svclassifier.support_vectors_)
 
-x = [15, 10]
-predicted_class = svclassifier.predict(x)
+predicted_class = svclassifier.predict([[8,8]])
+print(predicted_class)
+predicted_class = svclassifier.predict([[14,14]])
 print(predicted_class)
